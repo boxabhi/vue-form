e25a5e7ffb428470f1d2c4abc5c5830e4776f9f2
= <abhijeetg40@gmail.com>
2022-12-06 13:03:51 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,209 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div>
+  <TopNavBar :form-id="form.data[0].form"/>
+  
+  <v-container class="container">
+    <v-tabs v-model="tab">
+      <v-tab>Item One</v-tab>
+    <v-tab>Item Two</v-tab>
+   
+   
+  </v-tabs>
+  <v-tabs-items v-model="tab">
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text >Hi</v-card-text>
+        </v-card>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text >Hel;lo</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+
+    <v-card elevation="2" v-for="(q , i) in questions" :key="i" style="padding:30px">
+
+      <v-row align="center">
+        <v-col class="d-flex" cols="6" sm="6">
+          <v-text-field :label="questions[i].question" v-model="questions[i].question"></v-text-field>
+
+        </v-col>
+        <v-col class="d-flex" cols="6" sm="6">
+
+          <v-select v-model="q.question_type" :items="items" :value="q.question_type" label="Question Type">
+          </v-select>
+        </v-col>
+      </v-row>
+      <div v-if="q.question_type == 'short answer'">
+        <v-text-field label="Another input"></v-text-field>
+      </div>
+      <div v-if="q.question_type == 'paragraph'">
+        <v-textarea name="input-7-1" label="Default style"
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+          hint="Hint text"></v-textarea>
+      </div>
+
+      <div v-if="q.question_type == 'multiple choice'">
+        <v-row align="center" v-for="(option,j) in q.choices" :key="j">
+          <v-radio></v-radio>
+          <v-text-field :value="option.choice"></v-text-field>
+          <v-icon v-on:click="removeChoice(option.id)">mdi-close</v-icon>
+        </v-row>
+        <v-text-field placeholder="Add option" v-on:click="addOption(i)"></v-text-field>
+      </div>
+
+      <div v-if="q.question_type == 'checkbox'">
+        <v-row align="center" v-for="(option,i) in q.choices" :key="i">
+          <v-checkbox></v-checkbox>
+          <v-text-field :value="option.choice" v-on:input="editChoice"></v-text-field>
+          <v-icon v-on:click="removeChoice(option.id)">mdi-close</v-icon>
+          <v-text-field placeholder="Add option" v-on:click="addOption(i)"></v-text-field>
+        </v-row>
+
+      </div>
+
+
+
+    </v-card>
+
+
+    <v-btn style="margin-top: 40px;" p-4 elevation="2" v-on:click="addQuestion()">Click</v-btn>
+  </v-container>
+</div>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import TopNavBar from './TopNavBar.vue';
+  export default {
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    name: 'HelloWorld',
+    components: {
+    TopNavBar
+  },
+
+    data: () => ({
+      'items': ['short answer', 'multiple choice', 'checkbox', 'paragraph'],
+      'form': [],
+      'questions': [],
+      'question': {
+        'question': 'Untitled Question',
+        'options': ['Option 1'],
+        'question_type': 'radio'
+      },
+      tab: null,
+        tabs: [
+          'form', 'responses'
+        ],
+    }),
+    methods: {
+      editChoice(){
+        console.log('aa')
+      },
+      addOption(i) {
+        var _this = this
+      
+        console.log(_this.questions[i])
+
+
+        fetch('http://127.0.0.1:8000/choices/', {
+            method: 'post',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              'form': _this.form.data[0].form,
+              'question': _this.questions[i].id,
+              'choice': `Option ${_this.questions[i].choices.length}`
+            })
+          }).then(response => response.json())
+          .then(result => {
+
+            console.log(result)
+            _this.fetchForm()
+          })
+
+        this.questions[i].choices = [...this.questions[i].choices, {
+          'choice': "Option 1",
+          "id": 90,
+          "is_answer": false
+        }]
+
+      },
+      removeChoice(id) {
+        var _this = this
+        fetch('http://127.0.0.1:8000/choices/', {
+            method: 'delete',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              'id': id
+            })
+          }).then(response => response.json())
+          .then(result => {
+
+            console.log(result)
+            _this.fetchForm()
+          })
+      },
+      addQuestion() {
+        var question = {
+          "form": 13
+        }
+        var _this = this
+
+        fetch('http://127.0.0.1:8000/question/', {
+            method: 'post',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(question)
+          }).then(response => response.json())
+          .then(result => {
+            _this.questions.push(result.data)
+            console.log(result.data)
+          })
+
+        // this.questions.push(question)
+        console.log(this.question)
+      },
+
+      fetchForm() {
+        var _this = this
+        fetch(`http://127.0.0.1:8000/api/form/?id=13`)
+          .then(response => response.json())
+          .then(result => {
+            _this.form = result
+            _this.questions = result.data[0].questions
+            console.log(result.data[0].questions)
+          })
+      },
+
+
+      makeid(length = 5) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+      },
+      updateQuestion() {
+
+
+      }
+    },
+
+    created() {
+      this.fetchForm()
+    }
+
+  }
+</script>
---
+++ src/components/TopNavBar.vue
@@ -0,0 +1,48 @@
+<template>
+
+  <v-app-bar 
+     color="deep-purple" dark>
+    
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+    <v-toolbar-title   width="300">Title</v-toolbar-title>
+
+    <div    class="mx-auto " style="margin-left: 1000px !important">
+      <v-icon  color=" darken-2" class="m-3" style="margin: 20px;">
+      mdi-palette
+    </v-icon>
+   
+    <v-icon  color=" darken-2">
+      mdi-eye
+    </v-icon>
+    {{formId}}
+    <v-btn
+    style="margin: 20px;"
+  
+  depressed
+  class="text-dark m-3"
+  
+>Send</v-btn>
+    </div>
+  
+    
+  </v-app-bar>
+
+
+</template>
+
+<script>
+  export default {
+
+    props: {
+    formId: String
+  },
+    name: 'TopNavBar',
+
+
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+  }
+</script>
